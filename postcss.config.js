59435899e7bb60e61af45c917c14a511ee24afe4
Madhu Pandey <madhupandey29052004@gmail.com>
2025-09-18 17:36:23 +0530

    fix yash

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-// postcss.config.js
 module.exports = {
   plugins: {
-    "postcss-preset-env": {
-      stage: 3
-      // add any features you really need here
-    },
     tailwindcss: {},
-    autoprefixer: {}
-  }
+    autoprefixer: {},
+  },
 };
